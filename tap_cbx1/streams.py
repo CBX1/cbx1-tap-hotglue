fcc3200ba8f4b3f791b6ad779fb3d1109199fbea
nagumalli <vinay@different.ai>
2026-03-16 14:13:13 +0530

    accountStream adding

--- tap_cbx1/streams.py
+++ tap_cbx1/streams.py
@@ -1,12 +1,5 @@
 from tap_cbx1.client import CBX1Stream
 
-class AccountStream(CBX1Stream):
-    """Account stream with dynamic schema discovery."""
-    name = "accounts"
-    path = "/ACCOUNT"
-    target_name = "ACCOUNT"
-    primary_keys = ["id"]
-    replication_key = "updatedAt"
 
 
 
